f41ba872af1591bb9f06e092f077846a7b6aa261
Christopher Wells <christopherwells@posteo.net>
2023-11-11 08:06:22 +0000

    Formatting

--- pyrannosaur.py
+++ pyrannosaur.py
@@ -7,10 +7,10 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
 class ContentGenerator:
-    '''
+    """
     Generates content such as directories, pages, links and templates within
     .html files for new posts in a new or existing pyrannosaur website.
-    '''
+    """
 
     def __init__(self) -> None:
         self.dm = DirectoryManager()
@@ -21,9 +21,9 @@ class ContentGenerator:
 
     def convert_markdown_to_html(self) -> None:
         self.dm.check_for_valid_directory()
-        '''
+        """
         Go through /posts and convert any .md files to .html files.
-        '''
+        """
         # build file names
         md_files: list[str] = []
         for file in listdir("posts"):
@@ -42,38 +42,42 @@ class ContentGenerator:
         # write the files
         for i, file in enumerate(html_files):
             self.fm.write_to_file(
-                join("html", "posts"), file,
-                self.tl.base_template.render(title=f"{html_files[i]}",
-                                             content=md_content[i]))
+                join("html", "posts"),
+                file,
+                self.tl.base_template.render(
+                    title=f"{html_files[i]}", content=md_content[i]
+                ),
+            )
 
         # update archive.html
         archive: list[str] = []
         for file in html_files:
-            archive.append(
-                f"<a href=\"posts/{file}\">{file.split('.')[0]}</a>")
+            archive.append(f"<a href=\"posts/{file}\">{file.split('.')[0]}</a>")
         self.fm.write_to_file(
-            "html", "index.html",
-            self.tl.index_template.render(title="Site Index", archive=archive))
+            "html",
+            "index.html",
+            self.tl.index_template.render(title="Site Index", archive=archive),
+        )
 
 
 class FileManager:
-    '''
+    """
     Performs basic io for files such as reading and writing.
-    '''
+    """
 
     def __init__(self) -> None:
         pass
 
     def write_to_file(self, dir: str, file: str, operation) -> None:
         try:
-            with open(join(dir, file), 'w') as f:
+            with open(join(dir, file), "w") as f:
                 f.write(operation)
         except IOError as e:
             print(f"Error writing to {join(dir, file)}: {e}")
 
     def read_from_file(self, dir: str, file: str) -> str:
         try:
-            with open(join(dir, file), 'r') as f:
+            with open(join(dir, file), "r") as f:
                 data: str = f.read()
             return data
         except IOError as e:
@@ -86,20 +90,26 @@ class InvalidDirectoryError(Exception):
 
 
 class DirectoryManager:
-    '''
+    """
     Manages directories in a pyrannosaur website.
-    '''
+    """
 
     def __init__(self) -> None:
-        '''
+        """
         Initialise if posts/templates directories exist if they don't exist
         then you are in an invalid directory.
-        '''
+        """
         self.BASE_URL: str = getcwd()
-        self.POST_URL: str = join(self.BASE_URL, 'posts') if exists(
-            join(self.BASE_URL, 'posts')) else None
-        self.TEMPLATE_URL: str = join(self.BASE_URL, 'templates') if exists(
-            join(self.BASE_URL, 'templates')) else None
+        self.POST_URL: str = (
+            join(self.BASE_URL, "posts")
+            if exists(join(self.BASE_URL, "posts"))
+            else None
+        )
+        self.TEMPLATE_URL: str = (
+            join(self.BASE_URL, "templates")
+            if exists(join(self.BASE_URL, "templates"))
+            else None
+        )
         # if valid directory create the output directories
         if self.POST_URL is not None or self.TEMPLATE_URL is not None:
             if not exists("html"):
@@ -108,11 +118,11 @@ class DirectoryManager:
                 mkdir("html/posts")
 
     def check_for_valid_directory(self) -> None:
-        '''
+        """
         Check if you are in a valid directory.
         A valid directory is one containing /posts and /templates directories.
         If invalid, the program can raise an InvalidDirectoryError.
-        '''
+        """
         if self.POST_URL is None or self.TEMPLATE_URL is None:
             raise InvalidDirectoryError(
                 "The directory does not contain either /posts or /templates directory."
@@ -121,13 +131,14 @@ class DirectoryManager:
 
 
 class TemplateLoader:
-    '''
+    """
     Defines and loads templates for a pyrannosaur website.
-    '''
+    """
 
     def __init__(self) -> None:
-        self.env = Environment(loader=FileSystemLoader("templates"),
-                               autoescape=select_autoescape())
+        self.env = Environment(
+            loader=FileSystemLoader("templates"), autoescape=select_autoescape()
+        )
         self.base_template = self.env.get_template("base.html")
         self.index_template = self.env.get_template("index.html")
 
@@ -138,4 +149,3 @@ try:
 except InvalidDirectoryError as e:
     print(f"Error: {e}")
     # Handle the error, provide feedback, or exit the program
-
--- tests.py
+++ tests.py
@@ -11,18 +11,45 @@ class TestContentGenerator(unittest.TestCase):
     @patch("pyrannosaur.listdir", return_value=["file1.md", "file2.md"])
     @patch("pyrannosaur.markdown", return_value="mocked_html_content")
     @patch("pyrannosaur.FileManager.write_to_file")
-    def test_convert_markdown_to_html(self, mock_write_to_file, mock_markdown, mock_listdir, mock_join):
+    def test_convert_markdown_to_html(
+        self, mock_write_to_file, mock_markdown, mock_listdir, mock_join
+    ):
         cg = ContentGenerator()
 
         # Mock FileManager's read_from_file method
-        with patch("pyrannosaur.FileManager.read_from_file", return_value="mocked_markdown_content"):
+        with patch(
+            "pyrannosaur.FileManager.read_from_file",
+            return_value="mocked_markdown_content",
+        ):
             cg.convert_markdown_to_html()
 
         # Assertions
         expected_calls = [
-            call("html/posts", "file1.html", cg.tl.base_template.render(title="file1.html", content="mocked_html_content")),
-            call("html/posts", "file2.html", cg.tl.base_template.render(title="file2.html", content="mocked_html_content")),
-            call("html", "index.html", cg.tl.index_template.render(title="Site Index", archive=["<a href=\"posts/file1.html\">file1</a>", "<a href=\"posts/file2.html\">file2</a>"])),
+            call(
+                "html/posts",
+                "file1.html",
+                cg.tl.base_template.render(
+                    title="file1.html", content="mocked_html_content"
+                ),
+            ),
+            call(
+                "html/posts",
+                "file2.html",
+                cg.tl.base_template.render(
+                    title="file2.html", content="mocked_html_content"
+                ),
+            ),
+            call(
+                "html",
+                "index.html",
+                cg.tl.index_template.render(
+                    title="Site Index",
+                    archive=[
+                        '<a href="posts/file1.html">file1</a>',
+                        '<a href="posts/file2.html">file2</a>',
+                    ],
+                ),
+            ),
         ]
         mock_write_to_file.assert_has_calls(expected_calls, any_order=False)
 
@@ -32,7 +59,6 @@ class TestContentGenerator(unittest.TestCase):
             dm.check_for_valid_directory()
         except InvalidDirectoryError:
             pass
-
 
 
 if __name__ == "__main__":
